b0e50ba8ab2fdc49697cdfe5e7c1c441e7f50911
Michael Demmer <demmer@jut.io>
2016-03-09 10:32:38 -0800

    update the example code to the 0.5.x adapter api

--- test/read.spec.js
+++ test/read.spec.js
@@ -1,25 +1,26 @@
 'use strict';
 
 let expect = require('chai').expect;
-let juttle_test_utils = require('juttle/test/runtime/specs/juttle-test-utils');
+
+let juttle_test_utils = require('juttle/test').utils;
+let configureAdapter = juttle_test_utils.configureAdapter;
 let check_juttle = juttle_test_utils.check_juttle;
 
-let config = [];
-let adapter = require('../lib')(config, Juttle);
-let Juttle = require('juttle/lib/runtime').Juttle;
-Juttle.adapters.register(adapter.name, adapter);
+let config = {};
+config.path = path.resolve(__dirname, '..');
+configureAdapter({_ADAPTER_NAME_: config});
 
-describe('_ADAPTER_NAME_ source', () => {
-  
-    it('reads nothing back', () => {
-        return check_juttle({
-            program: 'read _ADAPTER_NAME_ '
-        })
-        .then(function(result) {
-            expect(result.errors.length).to.equal(0);
-            expect(result.warnings.length).to.equal(0);
-            expect(result.sinks.table[0].length).to.equal(0);
+juttle_test_utils.withAdapterAPI(() => {
+    describe('_ADAPTER_NAME_', () => {
+        it('reads nothing back', () => {
+            return check_juttle({
+                program: 'read _ADAPTER_NAME_ '
+            })
+            .then(function(result) {
+                expect(result.errors.length).to.equal(0);
+                expect(result.warnings.length).to.equal(0);
+                expect(result.sinks.table[0].length).to.equal(0);
+            });
         });
     });
-
 });
